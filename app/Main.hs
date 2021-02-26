12db1edc538f1808723e05f655380cdd07088a10
soeren berg <soeren.berg@protonmail.com>
2021-02-27 00:46:16 +0100

    read from args in main

--- app/Main.hs
+++ app/Main.hs
@@ -12,12 +12,9 @@ d = Date 2 2 4
 
 main :: IO ()
 main = do
-    -- (fileName:_) <- getArgs
-    let fileName = "example"
+    (fileName:_) <- getArgs
     input <- readFile fileName
-    -- putStrLn input
     let s = parse parseMessages "whatsapplog" input
-    -- B.putStrLn $ encode $ fromRight [] s
     B.putStrLn . encode . (fromRight []) $ s
 
 s = parse parseMessages "whatsapplog" <$> input
